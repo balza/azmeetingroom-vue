6afb12e91ed7a09b8dada5d2707580be08936532
paolo <paolo.balzarotti@gmail.com>
2017-03-17 13:07:22 +0100

    Improved ListaStanze layout

--- src/components/ListaStanze.vue
+++ src/components/ListaStanze.vue
@@ -3,14 +3,30 @@
     <table style="width:100%">
       <thead>
         <tr>
-          <th></th>
-          <th></th>
+          <th>Nome</th>
+          <th>Sede</th>
+          <th>Piano</th>
+          <th>Mattina</th>          
+          <th>Pomeriggio</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="sala in filteredSale(sale)">
-          <td>{{ sala.id }}</td>
           <td>{{ sala.text }}</td>
+          <td>{{ sala.sede }}</td>
+          <td>{{ sala.piano }}</td>
+          <td v-if='sala.liberaMattina == true'>
+            <a href='#'><span class="glyphicon glyphicon-ok" style="color:green"></span></a>
+          </td>
+          <td v-else>
+            <a href='#'><span class="glyphicon glyphicon-remove" style="color:red"></span></a>
+          </td>
+          <td v-if='sala.liberaPomeriggio == true'>
+            <a href='#'><span class="glyphicon glyphicon-ok" style="color:green"></span></a>
+          </td>
+          <td v-else>
+            <a href='#'><span class="glyphicon glyphicon-remove"style="color:red"></span></a>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -25,9 +41,11 @@ export default {
   data () {
     return {
       sale: [
-        { text: 'Sala 113', id: '113', idSede: 'oglio' },
-        { text: 'Sale Frigessi', id: 'frigessi', idSede: 'italia' },
-        { text: 'Sala Rita Levi Montalcini', id: 'srlm', idSede: 'brenta' }
+        { text: 'Sala 109', id: '113', idSede: 'oglio', sede: 'Oglio', piano: 1, liberaMattina: true, liberaPomeriggio: true },
+        { text: 'Sala 118', id: '118', idSede: 'oglio', sede: 'Oglio', piano: 1, liberaMattina: false, liberaPomeriggio: true },
+        { text: 'Sala 122', id: '122', idSede: 'oglio', sede: 'Oglio', piano: 1, liberaMattina: true, liberaPomeriggio: false },
+        { text: 'Sale Frigessi', id: 'frigessi', idSede: 'italia', sede: 'Corso Italia', piano: 0, liberaMattina: true, liberaPomeriggio: false },
+        { text: 'Sala Rita Levi Montalcini', id: 'srlm', idSede: 'brenta', sede: 'Oglio', piano: 1, liberaMattina: false, liberaPomeriggio: false }
       ]
     }
   },
@@ -44,5 +62,15 @@ export default {
 </script>
 
 <style>
+  th {
+    padding-top: 1cm;
+    text-align: center;
+    width: 200px;
+  }
+  td {
+    padding-top: 0.5cm;
+    text-align: center;
+    width: 200px;
+  }
 </style>
 
